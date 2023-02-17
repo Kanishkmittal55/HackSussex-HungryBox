ebfd9eac1d9df8ed0f56a6efa3fb19a424014085
Kanishk Mittal <87497747+Kanishkmittal55@users.noreply.github.com>
2023-02-17 22:29:23 +0000

    Express Setup githubToken Integration

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HackSussex-HungryBox
 HungryBox-Complete-Web-app-codespaces
+
+# Firstly we start by creating the Node - Express - MongoDB backend
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,10 @@
-console.log("Hello World")
+const express = require('express')
+const PORT = process.env.PORT;
+
+const app = express()
+
+app.get('/',(req,res) => {
+    res.json({message:'Welcome to the Smart Shleves API'})
+})
+
+app.listen(PORT, ()=> console.log(`Server started on PORT ${PORT}`))
