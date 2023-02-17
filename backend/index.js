63736bd22e258f55ca96d68bd707c7933a0f5b8c
Kanishk Mittal <87497747+Kanishkmittal55@users.noreply.github.com>
2023-02-17 23:56:32 +0000

    MVC setup complete

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,24 @@
 const express = require('express')
-const PORT = process.env.PORT;
-
+const colors= require('colors')
+const {errorHandler} = require('./middleware/errorMiddleware')
+const PORT = process.env.PORT || 5000;
+const connectDB = require('./config/db')
 const app = express()
 
+// Connect to Database
+connectDB()
+
+app.use(express.json())
+ 
+
 app.get('/',(req,res) => {
-    res.json({message:'Welcome to the Smart Shleves API'})
+    res.status(200).json({message:'Welcome to the Smart Shleves API'})
 })
 
-app.listen(PORT, ()=> console.log(`Server started on PORT ${PORT}`))
+// Routes
+app.use('/api/users',require('./Routes/userRoutes'))
+
+app.use(errorHandler)
+
+
+app.listen(PORT, ()=> console.log(`Server started on PORT ${PORT}`.yellow))
