55d3b177bf61ce6b673353ebb77e5e95a746aa80
kai7565105 <110289413+kai7565105@users.noreply.github.com>
2023-02-18 00:38:27 +0000

    User_2_commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+
+const User = require('../ models/userModel')
 
 
 // @desc Register a new user
@@ -6,15 +9,45 @@ const asyncHandler = require('express-async-handler')
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
     const {name, email, password} = req.body
-    console.log(name+ email+password)
+    
 
     // Validation
     if(!name || !email || !password){
         res.status(400)
         throw new Error('Please include all field')
     }
+
+    // Find if user already exists 
+    const userExists = await User.findOne({email})
+
+    if(userExists){
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password,salt)
+
+    // Create User
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        })
+    } else{
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
     
-    res.send('Register Route')
+   
 })
 
 // @desc Login a new user
